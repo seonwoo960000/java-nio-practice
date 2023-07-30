2fee6f1eb0a4335240a08bf05b5dec41a8bf2a1c
seonwoo960000 <seonwoo960000@naver.com>
2023-07-30 12:37:51 +0900

    Fix

--- src/Main.java
+++ src/Main.java
@@ -14,40 +14,40 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Select either one of the server or client to run the application.");
-        // createServer();
+         createServer();
         // startClient();
     }
 
     static void createServer() throws Exception {
-        Selector selector = Selector.open();
-
         ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-        serverSocketChannel.configureBlocking(false);
-        InetSocketAddress address = new InetSocketAddress(8080);
+        serverSocketChannel.configureBlocking(false); // non-blocking 동작을 위해 설정
+        InetSocketAddress address = new InetSocketAddress(8080); // localhost:8080
         ServerSocket serverSocket = serverSocketChannel.socket();
         serverSocket.bind(address);
 
+        // serverSocketChannel은 OP_ACCEPT 네트워크 이벤트를 모니터링
+        Selector selector = Selector.open();
         serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
 
         while (true) {
+            // 최소 1개 이상의 네트워크 이벤트(OP_ACCEPT, OP_READ)가 발생될때까지 blocking
             selector.select();
             Set<SelectionKey> selectedKeys = selector.selectedKeys();
 
+            // SelectionKey를 순회하면서 SelectionKey에 적합한 로직을 처리
             for (Iterator<SelectionKey> i = selectedKeys.iterator(); i.hasNext(); ) {
                 SelectionKey k = i.next();
                 if ((k.readyOps() & SelectionKey.OP_ACCEPT) == SelectionKey.OP_ACCEPT) {
-                    System.out.println("New TCP Connection");
-
+                    System.out.println("New TCP Connection.");
                     i.remove();
 
-                    // retrieve the SocketChannel for the new connection
-                    // use the same selector so now we can handle incoming data events on the same event loop
+                    // 새로운 TCP connection을 처리하기 위한 별도의 SocketChannel을 할당
                     ServerSocketChannel ssc = (ServerSocketChannel) k.channel();
                     SocketChannel sc = ssc.accept();
                     sc.configureBlocking(false);
+                    // socketChannel은 OP_READ 네트워크 이벤트를 모니터링
                     sc.register(selector, SelectionKey.OP_READ);
                 } else if ((k.readyOps() & SelectionKey.OP_READ) == SelectionKey.OP_READ) {
-                    // new data is available
                     i.remove();
 
                     SocketChannel sc = (SocketChannel) k.channel();
@@ -62,6 +62,9 @@ public class Main {
                         byte[] content = new byte[bytesRead];
                         buffer.get(content, 0, bytesRead);
                         System.out.println("Received: \n" + new String(content, StandardCharsets.UTF_8));
+
+                        ByteBuffer message = ByteBuffer.wrap(content);
+                        sc.write(message);
                     }
                 }
             }
@@ -69,19 +72,20 @@ public class Main {
     }
 
     static void startClient() throws Exception {
-        final Selector selector = Selector.open();
-
+        // Client용 SocketChannel 생성
         SocketChannel socketChannel = SocketChannel.open();
-        socketChannel.connect(new InetSocketAddress("localhost", 8080));
-        socketChannel.configureBlocking(false);
+        socketChannel.connect(new InetSocketAddress("localhost", 8080)); // localhost:8080
+        socketChannel.configureBlocking(false); // non-blocking
+        System.out.println("Socket connected");
 
-        System.out.println("Scoket connected");
-
+        // Selector 생성 후 OP_READ 등록
+        final Selector selector = Selector.open();
         socketChannel.register(selector, SelectionKey.OP_READ);
 
         new Thread(() -> {
             while (true) {
                 try {
+                    // OP_READ 이벤트를 모니터링. 서버로부터의 데이터 수신 시 아래 로직 실행
                     selector.select();
                     Set<SelectionKey> selectedKeys = selector.selectedKeys();
                     for (Iterator<SelectionKey> i = selectedKeys.iterator(); i.hasNext(); ) {
@@ -111,9 +115,11 @@ public class Main {
             }
         }, "selector thread").start();
 
+        // System.in에 입력된 메시지를 서버로 송신
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String line = scanner.nextLine();
+            // exit 입력 시 종료
             if (Objects.equals(line, "exit")) break;
 
             ByteBuffer buffer = ByteBuffer.allocate(1024);
